0628a2616d656b0116ef42b9c861861e3f11021f
hugjar1 <115717218+hugjar1@users.noreply.github.com>
2024-03-20 19:34:45 +0100

    w

--- socketfunction/socket/src/Chat.php
+++ socketfunction/socket/src/Chat.php
@@ -13,7 +13,7 @@ class Chat implements MessageComponentInterface {
     public function onOpen(ConnectionInterface $conn) {
         // Store the new connection to send messages to later
         $this->clients->attach($conn);
-        $conn->send("Username");
+        //$conn->send("Username");
 
         echo "New connection! ({$conn->resourceId})\n";
     }
